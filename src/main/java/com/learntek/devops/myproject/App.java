dc85ecedfe518bd065d19a77a02ad57650549f8c
sreekanthkonda007 <sreekanthkonda@gmail.com>
2017-04-15 12:52:45 -0400

    commit1

---
+++ src/main/java/com/learntek/devops/myproject/App.java
@@ -0,0 +1,17 @@
+package com.learntek.devops.myproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+System.out.println( "Hello DevOpsWorld!" );
+System.out.println( "Hello DevOpsWorld Diffrence!" );
+System.out.println( "Hello DevOpsWorld Develop branch!" );
+
+System.out.println( "Hello DevOpsWorld sprint1 branch intermediate changes..!" );    }
+}
